f527c243b4c6db74e25fad31e7cf93d5239ebba2
karrusb <karrusb@gmail.com>
2016-09-28 11:56:12 +0100

    Customer Entity class

---
+++ src/main/java/Customer.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+
+/**
+ * @author Administrator
+ *
+ */
+// Karrus  - customer class, setting/getting customer passwords and usernames
+public class Customer {
+
+	private String Username; 
+	private String Password; 
+	
+	public String getUsername (){
+		return Username;
+		
+	}
+	
+	public String getPassword (){
+		return Password;
+		
+	}
+	
+	public void setUsername (String vUsername) {
+		this.Username = vUsername;   
+	}
+	
+	public void setPassword (String vPassword) {
+		this.Password = vPassword;   
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
