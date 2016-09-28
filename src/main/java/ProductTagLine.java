d547f5032124d64e6651a7f97f59416986925b62
karrusb <karrusb@gmail.com>
2016-09-28 13:39:44 +0100

    Product Tag Line Added

---
+++ src/main/java/ProductTagLine.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+
+/**
+ * Karrus 28/09/2016
+ *
+ */
+public class ProductTagLine {
+	
+	private long productId;
+	private long tagId;
+	
+	
+	public void setProductId(long vproductId) {
+		this.productId = vproductId;
+	}
+	
+	public void setTagId(long vtagId) {
+		this.tagId = vtagId;
+	}
+	
+	
+	public long getProductId () {
+		
+		return productId;
+	}
+
+public long getTagId () {
+		
+		return tagId;
+	}
+
+}
