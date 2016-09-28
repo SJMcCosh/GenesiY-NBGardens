9d982e5dbde99c4033754c2d31b60fea0a0d3735
brianm89 <brianmcl89@gmail.com>
2016-09-28 11:45:13 +0100

    Adding Category and Tag skeleton classes

---
+++ src/main/java/Category.java
@@ -0,0 +1,16 @@
+
+public class Category 
+{
+	private String name;
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String vName)
+	{
+		this.name = vName;
+	}
+
+}
---
+++ src/main/java/Tag.java
@@ -0,0 +1,16 @@
+
+public class Tag 
+{
+	private String name;
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String vName)
+	{
+		this.name = vName;
+	}
+
+}
