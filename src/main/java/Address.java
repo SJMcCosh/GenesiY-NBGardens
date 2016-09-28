924eb3ba0347c7e1488bad61a8c6bb0e47533a65
SJMcCosh <sammccosh94@gmail.com>
2016-09-28 11:39:55 +0100

    Added Address class

---
+++ src/main/java/Address.java
@@ -0,0 +1,55 @@
+
+public class Address {
+
+	private int ID;
+	private String HouseNmNo, Street, TownCity, County, Postcode;
+
+	public void getID(int ID){
+		this.ID = ID;
+	}
+	
+	public void getHouse(String HouseNmNo) {
+		this.HouseNmNo = HouseNmNo;
+	}
+	
+	public void getStreet(String Street) {
+		this.Street = Street;
+	}
+	
+	public void getTownCity(String TownCity) {
+		this.TownCity = TownCity;
+	}
+	
+	public void getCounty(String County) {
+		this.County = County;
+	}
+	
+	public void getPostcode(String Postcode) {
+		this.Postcode = Postcode;
+	}
+	
+	public int ID() {
+		return ID;
+	}
+	
+	public String getHouse() {
+		return HouseNmNo;
+	}
+	
+	public String getStreet() {
+		return Street;
+	}
+	
+	public String getTownCity() {
+		return TownCity;
+	}
+	
+	public String getCounty() {
+		return County;
+	}
+	
+	public String getPostcode() {
+		return Postcode;
+	}
+	
+}
