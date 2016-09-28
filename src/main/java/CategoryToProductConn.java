7e785caaa2e021e6653eb62730a0752086baa60c
samwdp <samwdp@gmail.com>
2016-09-28 14:49:06 +0100

    Changed a connection

--- src/main/java/CategoryToProductConn.java
+++ src/main/java/CategoryToProductConn.java
@@ -8,13 +8,26 @@ import javax.validation.constraints.NotNull;
  */
 public class CategoryToProductConn {
 	@ManyToOne
-	@Id
 	@JoinColumn (name = "productID", nullable = false)
 	@NotNull
 	private Product product;
 	@ManyToOne
-	@Id
 	@JoinColumn (name = "category", nullable = false)
 	@NotNull
 	private Category category;
+	
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+	
 }
--- src/main/java/ProductTagLine.java
+++ src/main/java/ProductTagLine.java
@@ -3,40 +3,39 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
 /**
  * Karrus 28/09/2016
  *
  */
 
 @Entity
-@Table (name = "productTagLine")
+@Table(name = "productTagLine")
 public class ProductTagLine {
-	
+
 	@ManyToOne
-	@JoinColumn (name = "productID_fk", nullable = false)
+	@JoinColumn(name = "productID_fk", nullable = false)
 	private long productId;
-	
+
 	@OneToOne
-	@JoinColumn (name = "productID_fk", nullable = false)
+	@JoinColumn(name = "productID_fk", nullable = false)
 	private long tagId;
-	
-	
+
 	public void setProductId(long vproductId) {
 		this.productId = vproductId;
 	}
-	
+
 	public void setTagId(long vtagId) {
 		this.tagId = vtagId;
 	}
-	
-	
-	public long getProductId () {
-		
+
+	public long getProductId() {
+
 		return productId;
 	}
 
-public long getTagId () {
-		
+	public long getTagId() {
+
 		return tagId;
 	}
 
