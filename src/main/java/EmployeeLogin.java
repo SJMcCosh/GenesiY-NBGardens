be28171589d90ca2f211ce4ef424d237567c6f76
samwdp <samwdp@gmail.com>
2016-09-28 13:13:12 +0100

    Added outline for the employee login

---
+++ src/main/java/EmployeeLogin.java
@@ -0,0 +1,26 @@
+/**
+ * Outline for the Employee login
+ * @author Sam Precious
+ *
+ */
+public class EmployeeLogin {
+	private String login;
+	private String password;
+	
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
