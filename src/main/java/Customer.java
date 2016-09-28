dd7149c08ebc69bd24ef23f31c0c643d14add42f
karrusb <karrusb@gmail.com>
2016-09-28 14:23:26 +0100

    Customer added anotations corrected

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,17 +1,11 @@
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.NotNull;
-import javax.persistence.Size;
+import javax.persistence.*;
+import javax.validation.constraints.*;
 
 
 
 /**
- * customer class, setting/getting customer passwords and usernames
- * @author Karrus
+ * 
  */
-<<<<<<< HEAD
 
 /**
  * @author Administrator
@@ -22,39 +16,39 @@ import javax.persistence.Size;
 @Table (name = "customer")
 public class Customer {
 	@Id
-	@Column (name = "username")
-	@NotNull
+	@Column (name = "username", nullable = false)
 	@Size (min = 5, max = 225)
 	private String Username; 
 	
 	
-	@Column (name = "password")
-	@NotNull
+	@Column (name = "password", nullable = false)
 	@Size (min = 5, max = 225)
 	private String Password; 
 	
 	public String getUsername (){
 		return Username;
 		
-=======
-public class Customer {
-
-	private String username; 
-	private String password;
-	public String getUsername() {
-		return username;
->>>>>>> e5a3b367b1b0e6163385a168cc80f62e68ca7c20
 	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	} 
 	
+	public String getPassword (){
+		return Password;
+		
+	}
+	
+	public void setUsername (String vUsername) {
+		this.Username = vUsername;   
+	}
+	
+	public void setPassword (String vPassword) {
+		this.Password = vPassword;   
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 	
 	
 
