2c4ef5b9745111b339f13bf92d8983656bdb3951
samwdp <samwdp@gmail.com>
2016-09-28 11:48:33 +0100

    Added prodict basic class outline

---
+++ src/main/java/Product.java
@@ -0,0 +1,43 @@
+/**
+ * Product outline
+ * @author Sam Precious
+ *
+ */
+public class Product {
+	
+	private String name;
+	private int averageRating;
+	private String description;
+	private String size;
+	private double weight;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAverageRating() {
+		return averageRating;
+	}
+	public void setAverageRating(int averageRating) {
+		this.averageRating = averageRating;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+}
