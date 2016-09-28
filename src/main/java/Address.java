1fd3d4063d4845661551f805a3a3ce8991732aee
SJMcCosh <sammccosh94@gmail.com>
2016-09-28 13:40:47 +0100

    Updated Address

--- src/main/java/Address.java
+++ src/main/java/Address.java
@@ -5,61 +5,51 @@
  */
 public class Address {
 
-	private int AddressID;
-	private String HouseNmNo;
-	private String Street;
-	private String TownCity;
-	private String County;
-	private String Postcode;
-
-	public void getAddressID(int AddressID){
-		this.AddressID = AddressID;
+	private int addressID;
+	private String houseNmNo;
+	private String street;
+	private String townCity;
+	private String county;
+	private String postcode;
+	public int getAddressID() {
+		return addressID;
 	}
-	
-	public void getHouse(String HouseNmNo) {
-		this.HouseNmNo = HouseNmNo;
+	public void setAddressID(int addressID) {
+		this.addressID = addressID;
 	}
-	
-	public void getStreet(String Street) {
-		this.Street = Street;
+	public String getHouseNmNo() {
+		return houseNmNo;
 	}
-	
-	public void getTownCity(String TownCity) {
-		this.TownCity = TownCity;
+	public void setHouseNmNo(String houseNmNo) {
+		this.houseNmNo = houseNmNo;
 	}
-	
-	public void getCounty(String County) {
-		this.County = County;
-	}
-	
-	public void getPostcode(String Postcode) {
-		this.Postcode = Postcode;
-	}
-	
-	public int AddressID() {
-		return AddressID;
-	}
-	
-	public String getHouse() {
-		return HouseNmNo;
-	}
-	
 	public String getStreet() {
-		return Street;
+		return street;
 	}
-	
+	public void setStreet(String street) {
+		this.street = street;
+	}
 	public String getTownCity() {
-		return TownCity;
+		return townCity;
 	}
-	
+	public void setTownCity(String townCity) {
+		this.townCity = townCity;
+	}
 	public String getCounty() {
-		return County;
+		return county;
+	}
+	public void setCounty(String county) {
+		this.county = county;
+	}
+	public String getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
 	}
 	
-	public String getPostcode() {
-		return Postcode;
-	}
 	/**
 	 * Add code to pull from database
 	 */
+	
 }
