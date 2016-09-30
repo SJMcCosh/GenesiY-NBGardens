8328aed81f181eb06bc0f21eb605bdf9cab9f3a1
Samuel McCosh <sammccosh94@gmail.com>
2016-09-30 14:24:27 +0100

    Update ReviewRepository.java

--- src/main/java/com/genesisY/nbGardensCatalogue/entityManagers/ReviewRepository.java
+++ src/main/java/com/genesisY/nbGardensCatalogue/entityManagers/ReviewRepository.java
@@ -1,3 +1,7 @@
+/**
+ * Class that is a repository for the Reviews Entities
+ * @author Samuel McCosh
+ */
 package com.genesisY.nbGardensCatalogue.entityManagers;
 
 public interface ReviewRepository {
