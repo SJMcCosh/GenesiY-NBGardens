427309b66afc8805a506ba44e493bb9f38c08f96
brianm89 <brianmcl89@gmail.com>
2016-09-30 14:07:53 +0100

    Adding Tag Repository interface and Offline Manager

---
+++ src/main/java/com/genesisY/nbGardensCatalogue/entityManagers/TagManagerOffline.java
@@ -0,0 +1,24 @@
+package com.genesisY.nbGardensCatalogue.entityManagers;
+
+import com.genesisY.nbGardensCatalogue.entities.Product;
+import com.genesisY.nbGardensCatalogue.entities.Tag;
+
+public class TagManagerOffline implements TagRepository 
+{
+	
+	public void createTag(Tag a)
+	{
+		
+	}
+	
+	public void readTagByProductID(Product id)
+	{
+		
+	}
+	
+	public void updateTagWithProduct(Tag a, Product id)
+	{
+		
+	}
+
+}
