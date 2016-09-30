001d4ba7fe9bf2469be80c30398679d0d32ee35c
TChats <thomas.chatterjee@hotmail.co.uk>
2016-09-30 14:55:24 +0100

    Generic message

--- src/main/java/com/genesisY/nbGardensCatalogue/entityManagers/offline/PurchaseOrderLineManagerOffline.java
+++ src/main/java/com/genesisY/nbGardensCatalogue/entityManagers/offline/PurchaseOrderLineManagerOffline.java
@@ -3,24 +3,24 @@ package com.genesisY.nbGardensCatalogue.entityManagers.offline;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import com.genesisY.nbGardensCatalogue.entities.PurchaseOrderLine;
 import com.genesisY.nbGardensCatalogue.entityManagers.repositories.PurchaseOrderLineRepository;
+import com.genesisY.nbGardensCatalogue.initialData.InitialData;
 
 public class PurchaseOrderLineManagerOffline implements PurchaseOrderLineRepository{
-
+	@Inject
+	private InitialData initialData;
+	
 	public void createPurchaseOrderLine(PurchaseOrderLine pol) {
 		// adds a purchase order line to the db
-		
-	}
-
-	public void createPurchaseOrderLines(List<PurchaseOrderLine> pol) {
-		// adds many purchase order lines to the db
-		
+		initialData.addPurchaseOrderLine(pol);
 	}
 
 	public ArrayList<PurchaseOrderLine> readPurchaseOrderLine() {
 		// reads a list of purchase order lines from the db
-		return null;
+		return initialData.getPolList();
 	}
 
 	public void updatePurchaseOrderLine(PurchaseOrderLine pol) {
@@ -31,6 +31,11 @@ public class PurchaseOrderLineManagerOffline implements PurchaseOrderLineReposit
 	public void removePurchaseOrderLine(PurchaseOrderLine pol) {
 		/* deletes a purchase order line on the db
 		only to be usable on POLs that are on orders yet to be implemented*/
+		
+	}
+
+	public void createPurchaseOrderLines(List<PurchaseOrderLine> pol) {
+		// TODO Auto-generated method stub
 		
 	}
 
