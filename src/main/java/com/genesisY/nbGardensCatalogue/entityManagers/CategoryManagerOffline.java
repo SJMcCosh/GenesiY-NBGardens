a2adf34da26a3f8ee3a8a6b74e02a7507f133e7c
SJMcCosh <sammccosh94@gmail.com>
2016-09-30 13:01:03 +0100

    Added Category manager

--- src/main/java/com/genesisY/nbGardensCatalogue/entityManagers/CategoryManagerOffline.java
+++ src/main/java/com/genesisY/nbGardensCatalogue/entityManagers/CategoryManagerOffline.java
@@ -5,11 +5,27 @@ import com.genesisY.nbGardensCatalogue.entities.Category;
 public class CategoryManagerOffline implements CategoryRepository {
 
 	public void createCategory(Category x){
-		
+		// add category to database
+		// categoryID = generated number 
+		// categoryName = input
 	}
 	
 	public void readCategory(Category x){
-		
+		// read category from database
+		// search for categoryID from input
+		// display category name
+	}
+	
+	public void updateCategory(Category x){
+		// search for categoryID from input
+		// new name = input
+		// display category new name
+	}
+	
+	public void archiveCategory(Category x){
+		// search for categoryID from input
+		// set the status of the category to "Inactive"
+		// display confirmation message
 	}
 	
 }
