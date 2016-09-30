7f9224ab272f882413f6ec0bd59dae862347db4e
SJMcCosh <sammccosh94@gmail.com>
2016-09-30 11:47:36 +0100

    Added Category manager

---
+++ src/main/java/com/genesisY/nbGardensCatalogue/entityManagers/CategoryManagerOffline.java
@@ -0,0 +1,15 @@
+package com.genesisY.nbGardensCatalogue.entityManagers;
+
+import com.genesisY.nbGardensCatalogue.entities.Category;
+
+public class CategoryManagerOffline implements CategoryRepository {
+
+	public void createCategory(Category x){
+		
+	}
+	
+	public void readCategory(Category x){
+		
+	}
+	
+}
