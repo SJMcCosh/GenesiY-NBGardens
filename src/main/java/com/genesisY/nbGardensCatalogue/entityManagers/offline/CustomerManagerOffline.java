1a2b9076bfcfc0348519c18a0d0a9a37e72b0d67
brianm89 <brianmcl89@gmail.com>
2016-10-10 16:34:17 +0100

    minor changes to customermanager

--- src/main/java/com/genesisY/nbGardensCatalogue/entityManagers/offline/CustomerManagerOffline.java
+++ src/main/java/com/genesisY/nbGardensCatalogue/entityManagers/offline/CustomerManagerOffline.java
@@ -9,12 +9,25 @@ package com.genesisY.nbGardensCatalogue.entityManagers.offline;
 import javax.ejb.Stateless;
 
 import com.genesisY.nbGardensCatalogue.entities.Customer;
+import com.genesisY.nbGardensCatalogue.entityManagers.repositories.CustomerRepository;
 
 @Stateless
 public class CustomerManagerOffline implements CustomerRepository
 {
 	//Read
 	public void readCustomerDetails(Customer c)
+	{
+		
+	}
+
+	@Override
+	public void updateAddressDetails(Customer c) 
+	{
+		
+	}
+
+	@Override
+	public void updateTelephoneNumber(Customer c) 
 	{
 		
 	}
