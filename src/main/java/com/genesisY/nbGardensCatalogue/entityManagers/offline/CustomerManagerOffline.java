a6d4bf5a80f4bb44b0c9ee4d6c2c8c027e5c1d96
SJMcCosh <sammccosh94@gmail.com>
2016-10-10 16:24:48 +0100

    Added the category html and css

--- src/main/java/com/genesisY/nbGardensCatalogue/entityManagers/offline/CustomerManagerOffline.java
+++ src/main/java/com/genesisY/nbGardensCatalogue/entityManagers/offline/CustomerManagerOffline.java
@@ -9,6 +9,7 @@ package com.genesisY.nbGardensCatalogue.entityManagers.offline;
 import javax.ejb.Stateless;
 
 import com.genesisY.nbGardensCatalogue.entities.Customer;
+import com.genesisY.nbGardensCatalogue.entityManagers.repositories.CustomerRepository;
 
 @Stateless
 public class CustomerManagerOffline implements CustomerRepository
@@ -16,6 +17,16 @@ public class CustomerManagerOffline implements CustomerRepository
 	//Read
 	public void readCustomerDetails(Customer c)
 	{
+		
+	}
+
+	public void updateAddressDetails(Customer c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateTelephoneNumber(Customer c) {
+		// TODO Auto-generated method stub
 		
 	}
 }
