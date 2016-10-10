21e9df3d51733096cdfbb7d7bdfff932e096ebdf
samwdp <samwdp@gmail.com>
2016-10-10 16:32:52 +0100

    Updated the page

--- src/main/java/com/genesisY/nbGardensCatalogue/entityManagers/offline/ProductsVariantsManagerOffline.java
+++ src/main/java/com/genesisY/nbGardensCatalogue/entityManagers/offline/ProductsVariantsManagerOffline.java
@@ -9,61 +9,44 @@ import javax.ejb.Stateless;
 @Stateless
 public class ProductsVariantsManagerOffline implements ProductsVariantsRepository{
 
-	@Override
 	public double readStockLevels(int prodVarId) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
 	public String readProductStatus(int prodVarId) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void updateProductStock(int stockLevel) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void updateProductStatus(String status) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public double readProductPrice(int prodVarId) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
 	public void updateProductPrice(double price) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public ArrayList readProductVariant(int prodVarId) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void createProductVariant(ArrayList prodVaraiant) {
 		// TODO Auto-generated method stub
 		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
