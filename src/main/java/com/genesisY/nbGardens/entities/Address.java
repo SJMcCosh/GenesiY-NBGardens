f4a0b020dd77fe469450a09b05893b7b8576be18
TChats <thomas.chatterjee@hotmail.co.uk>
2016-10-24 12:16:07 +0100

    supplier initial data added and supplier xhtml

--- src/main/java/com/genesisY/nbGardens/entities/Address.java
+++ src/main/java/com/genesisY/nbGardens/entities/Address.java
@@ -17,24 +17,34 @@ public class Address {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int addressID;
 
-	@Column(name = "houseNmNo", nullable = false, length = 120)
+	@Column(name = "addressLine1", nullable = false, length = 120)
 	@Size(min = 1, max = 120)
-	private String houseNmNo;
+	@NotNull
+	private String addressLine1;
 
-	@Column(name = "street", nullable = false, length = 70)
+	@Column(name = "addressLine2", nullable = false, length = 70)
 	@Size(min = 5, max = 70)
-	private String street;
+	private String addressLine2;
+	
+	@Column(name = "addressLine3", nullable = false, length = 70)
+	@Size(min = 5, max = 70)
+	private String addressLine3;
 
 	@Column(name = "townCity", nullable = false, length = 58)
 	@Size(min = 2, max = 58)
+	@NotNull
 	private String townCity;
 
 	@Column(name = "county", nullable = false, length = 35)
 	@Size(min = 4, max = 35)
 	private String county;
+	
+	@Column(name = "country")
+	private String country;
 
 	@Column(name = "postcode", nullable = false, length = 9)
 	@Size(min = 7, max = 9)
+	@NotNull
 	private String postcode;
 
 	@OneToMany
@@ -46,13 +56,13 @@ public class Address {
 
 	}
 
-	public Address(String houseNmNo, String street, String townCity, String county, String postcode, Customer customer) {
-		this.houseNmNo = houseNmNo;
-		this.street = street;
+	public Address(String addressLine1, String addressLine2, String addressLine3, String townCity, String country, String postcode) {
+		this.addressLine1 = addressLine1;
+		this.addressLine2 = addressLine2;
+		this.addressLine3 = addressLine3;
 		this.townCity = townCity;
-		this.county = county;
+		this.county = country;
 		this.postcode = postcode;
-		this.customer = customer;
 	}
 
 	public int getAddressID() {
@@ -63,20 +73,28 @@ public class Address {
 		this.addressID = addressID;
 	}
 
-	public String getHouseNmNo() {
-		return houseNmNo;
+	public String getAddressLine1() {
+		return addressLine1;
 	}
 
-	public void setHouseNmNo(String houseNmNo) {
-		this.houseNmNo = houseNmNo;
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
 	}
 
-	public String getStreet() {
-		return street;
+	public String getAddressLine2() {
+		return addressLine2;
 	}
 
-	public void setStreet(String street) {
-		this.street = street;
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+	
+	public String getAddressLine3() {
+		return addressLine2;
+	}
+
+	public void setAddressLine3(String addressLine3) {
+		this.addressLine3 = addressLine3;
 	}
 
 	public String getTownCity() {
@@ -93,6 +111,14 @@ public class Address {
 
 	public void setCounty(String county) {
 		this.county = county;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
 	}
 
 	public String getPostcode() {
--- src/main/java/com/genesisY/nbGardens/initialData/ImsInitialData.java
+++ src/main/java/com/genesisY/nbGardens/initialData/ImsInitialData.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 
 import javax.annotation.*;
 
+import com.genesisY.nbGardens.entities.Address;
 import com.genesisY.nbGardens.entities.Product;
+import com.genesisY.nbGardens.entities.Supplier;
 
 public class ImsInitialData {
 	
 	private ArrayList<Product> prodList = new ArrayList<Product>();
+	private ArrayList<Supplier> supplierList = new ArrayList<Supplier>();
 	
 	
 	
@@ -16,7 +19,16 @@ public class ImsInitialData {
 	public void setupData()
 	{
 		prodList.add(new Product("Game of Gnomes", 4.5, "A GOT gnome", "No idea", 15.0));
-		prodList.add(new Product("random gnome", 2.5, "lelelelel", "woah a second gnome", 10.0));		
+		prodList.add(new Product("random gnome", 2.5, "lelelelel", "woah a second gnome", 10.0));
+		Address address1 = new Address("32 Fine Strasse", "", "", "Berlin", "Germany", "54624");
+		Supplier supplier1 = new Supplier("Gnomes4U", "05853123673", "gnomes4u@gnomes4u.com", address1);
+		Address address2 = new Address("2546 New Road", "", "", "Paris", "France", "75003");
+		Supplier supplier2 = new Supplier("Gnomeo's Gnomes", "12354234723", "gnomeo@gnomeosgnomes.com", address2);
+		Address address3 = new Address("21 Gnome Street", "", "", "Saariselka", "Finland", "99830 Saariselka");
+		Supplier supplier3 = new Supplier("Gnome Adventures", "34252824665", "gnomes@gnomeadventures.com", address3);
+		supplierList.add(supplier1);
+		supplierList.add(supplier2);
+		supplierList.add(supplier3);
 	}
 	
 	
@@ -29,6 +41,17 @@ public class ImsInitialData {
 	public ArrayList<Product> getProdList()
 	{
 		return this.prodList;
+	}
+	
+	public void setSupplierList(ArrayList<Supplier> supplierList)
+	{
+		this.supplierList = supplierList;
+	}
+	
+	
+	public ArrayList<Supplier> getSupplierList()
+	{
+		return this.supplierList;
 	}
 	
 	
