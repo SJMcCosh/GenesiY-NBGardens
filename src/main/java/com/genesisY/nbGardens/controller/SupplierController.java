473c8fd40da969a35eb8df58e931b403203938e2
TChats <thomas.chatterjee@hotmail.co.uk>
2016-10-24 17:12:23 +0100

    view supplier getting there

--- src/main/java/com/genesisY/nbGardens/controller/SupplierController.java
+++ src/main/java/com/genesisY/nbGardens/controller/SupplierController.java
@@ -6,10 +6,13 @@ import javax.enterprise.context.SessionScoped;
 import javax.faces.model.DataModel;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.faces.model.ListDataModel;
 
 import com.genesisY.nbGardens.entities.Supplier;
 import com.genesisY.nbGardens.services.SupplierService;
+
+import javax.faces.model.ListDataModel;
+
+
 
 @Named ("suppliers")
 @SessionScoped
@@ -20,6 +23,7 @@ public class SupplierController implements Serializable {
 	private DataModel<Supplier> datamodel; 
 	
 	public String viewSuppliers(){
+		System.out.println(">>>>>>>>>");
 		datamodel = new ListDataModel<Supplier>(supplierService.viewSuppliers());
 		return "viewsupplier"; 
 		
