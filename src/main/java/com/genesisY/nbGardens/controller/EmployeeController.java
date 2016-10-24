f406a8552cad88ed978bfc659443ef6b82363d2b
samwdp <samwdp@gmail.com>
2016-10-24 12:36:33 +0100

    Addeded login functionality

---
+++ src/main/java/com/genesisY/nbGardens/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.genesisY.nbGardens.controller;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.genesisY.nbGardensCatalogue.entities.Employee;
+
+@SuppressWarnings("serial")
+@Named("employee")
+@SessionScoped
+public class EmployeeController implements Serializable {
+
+	@Inject
+	private Employee employee;
+
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+}
---
+++ src/main/java/com/genesisY/nbGardens/controller/LoginController.java
@@ -0,0 +1,62 @@
+package com.genesisY.nbGardens.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.genesisY.nbGardens.services.LoginService;
+
+@Named("login")
+@RequestScoped
+public class LoginController{
+
+	@Inject
+	private LoginService passcheck;
+	@Inject
+	private UserCredentials userCredentials;
+	@Inject
+	private EmployeeController employeeController;
+	private String username = "";
+	private String password = "";
+	private String error = "";
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String login() {
+		
+		if (username.equals("")) {
+			error = "Please enter a username";
+			password = "";
+			return "loginpage";
+		}
+		else if (password.equals("")) {
+			error = "Please enter a password";
+			password = "";
+			return "loginpage";
+		}
+		else if (passcheck.passCheck(username, password) == true) {
+			userCredentials.setUsername(username);
+			userCredentials.setLoggedin(passcheck.passCheck(username, password));
+			return "home";
+		} else {
+			error = "Invalid username and password";
+			username = "";
+			password = "";
+			return "loginpage";
+		}
+	}
+}
