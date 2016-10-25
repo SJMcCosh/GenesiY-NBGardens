5c4507f2867a436e8fbd2c9ada1d546e0ec71498
samwdp <samwdp@gmail.com>
2016-10-25 14:19:09 +0100

    Error fixing attempt

--- src/main/java/com/genesisY/nbGardens/controller/SupplierController.java
+++ src/main/java/com/genesisY/nbGardens/controller/SupplierController.java
@@ -1,22 +1,17 @@
 package com.genesisY.nbGardens.controller;
 
-import java.io.Serializable;
-
-import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.genesisY.nbGardens.entities.Supplier;
 import com.genesisY.nbGardens.services.SupplierService;
 
-import javax.faces.model.ListDataModel;
-
-
-
 @Named ("suppliers")
-@SessionScoped
-public class SupplierController implements Serializable {
+@RequestScoped
+public class SupplierController {
 
 	@Inject 
 	private SupplierService supplierService; 
@@ -24,20 +19,7 @@ public class SupplierController implements Serializable {
 	
 	public String viewSuppliers(){
 		System.out.println(">>>>>>>>>");
-		setDatamodel(new ListDataModel<Supplier>(supplierService.viewSuppliers()));
-		return "viewsupplier"; 
-		
+		datamodel = new ListDataModel<Supplier>(supplierService.viewSuppliers());
+		return "viewsupplier"; 	
 	}
-
-	public DataModel<Supplier> getDatamodel() {
-		return datamodel;
-	}
-
-	public void setDatamodel(DataModel<Supplier> datamodel) {
-		this.datamodel = datamodel;
-	}
-	
-	
-	
-	
 }
