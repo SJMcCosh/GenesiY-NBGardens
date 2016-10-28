b5795a27ed3a627d6e13f176461994f5f208a678
samwdp <samwdp@gmail.com>
2016-10-28 16:05:22 +0100

    Updated template

---
+++ src/main/java/com/genesisY/nbGardens/services/SearchService.java
@@ -0,0 +1,14 @@
+package com.genesisY.nbGardens.services;
+
+import java.util.List;
+
+import com.genesisY.nbGardens.entities.Product;
+
+public class SearchService {
+
+	public List<Product> prodSearch(String term) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
