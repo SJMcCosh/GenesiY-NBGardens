c1c8d386c4fc576cc8871cb9ebbde486392afd7c
SJMcCosh <sammccosh94@gmail.com>
2016-11-02 14:24:51 +0000

    Uploading changes to basket

---
+++ src/main/java/com/genesisY/nbGardens/controller/AddToBasketController.java
@@ -0,0 +1,25 @@
+package com.genesisY.nbGardens.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.genesisY.nbGardens.services.BasketService;
+
+@Named("addBasket")
+@RequestScoped
+public class AddToBasketController {
+	
+	@Inject private ProductsController productsController;
+	@Inject private BasketService basketService;
+	
+	/**
+	 * Sends the product that the user has clicked to the data store for the basket
+	 */
+	public void addToBasket(){
+		
+		basketService.addProductToBasket(productsController.getProduct(), productsController.getQuantityOfItemsSelected());
+		
+	}
+	
+}
