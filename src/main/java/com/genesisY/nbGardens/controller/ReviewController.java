bd2df513e82adc3b4ff405fcefce4782a13e8171
samwdp <samwdp@gmail.com>
2016-11-02 17:16:16 +0000

    Dunno what happened with the last commit, didn't add this file

---
+++ src/main/java/com/genesisY/nbGardens/controller/ReviewController.java
@@ -0,0 +1,36 @@
+package com.genesisY.nbGardens.controller;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.genesisY.nbGardensCatalogue.entities.Review;
+
+@Named("reviewcon")
+@RequestScoped
+public class ReviewController {
+
+	@Inject private ProductsController productsController;
+	private DataModel<Review> dataModel = null;
+	
+	
+	public void getReviews(){
+		
+		dataModel = new ListDataModel<Review>(productsController.getProduct().getReviews());
+		
+	}
+
+
+	public DataModel<Review> getDataModel() {
+		return dataModel;
+	}
+
+
+	public void setDataModel(DataModel<Review> dataModel) {
+		this.dataModel = dataModel;
+	}
+	
+	
+	
+}
