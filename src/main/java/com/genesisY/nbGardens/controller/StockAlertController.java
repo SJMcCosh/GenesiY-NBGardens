5f06f4863003d01317072437ef13480257bb0779
brianm89 <brianmcl89@gmail.com>
2016-11-03 17:17:11 +0000

    changes to stockalert

--- src/main/java/com/genesisY/nbGardens/controller/StockAlertController.java
+++ src/main/java/com/genesisY/nbGardens/controller/StockAlertController.java
@@ -1,5 +1,38 @@
 package com.genesisY.nbGardens.controller;
 
-public class StockAlertController {
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.genesisY.nbGardens.entities.StockAlert;
+import com.genesisY.nbGardens.services.StockAlertService;
+
+@SuppressWarnings("serial")
+@Named("stockAlerts")
+@SessionScoped
+public class StockAlertController implements Serializable {
+	
+	@Inject
+	private StockAlertService saService;
+	
+	private DataModel<StockAlert> dataModel;
+	
+	public String onLoad()
+	{
+		setDataModel(new ListDataModel<StockAlert>(saService.getAllStockAlerts()));
+		return "index";
+	}
+
+	public DataModel<StockAlert> getDataModel() {
+		return dataModel;
+	}
+
+	public void setDataModel(DataModel<StockAlert> dataModel) {
+		this.dataModel = dataModel;
+	}
 
 }
--- src/main/java/com/genesisY/nbGardens/services/StockAlertService.java
+++ src/main/java/com/genesisY/nbGardens/services/StockAlertService.java
@@ -1,5 +1,30 @@
 package com.genesisY.nbGardens.services;
 
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import com.genesisY.nbGardens.entities.StockAlert;
+import com.genesisY.nbGardens.entityManagers.StockAlertManager;
+
+@Stateless
 public class StockAlertService {
+	
+	@Inject
+	private StockAlertManager saManager;
+	
+	public List<StockAlert> getAllStockAlerts()
+	{
+		if (saManager.getAllStockAlerts() != null)
+		{
+			return saManager.getAllStockAlerts();
+		}
+		else
+		{
+			return null;
+		}
+	}
+	
 
 }
