06fb4ec4f5231c324b12f36b515b68d70c2fe096
karrusb <karrusb@gmail.com>
2016-11-07 14:40:57 +0000

    commit before pull

--- src/main/java/com/genesisY/nbGardens/entityManagers/offline/StockAlertManagerOffline.java
+++ src/main/java/com/genesisY/nbGardens/entityManagers/offline/StockAlertManagerOffline.java
@@ -2,12 +2,16 @@ package com.genesisY.nbGardens.entityManagers.offline;
 
 import java.util.List;
 
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 
 import com.genesisY.nbGardens.entities.Notification;
 import com.genesisY.nbGardens.entityManagers.NotificationManager;
 import com.genesisY.nbGardens.initialData.ImsInitialData;
 
+@Default
+@Stateless
 public class StockAlertManagerOffline implements NotificationManager {
 	
 	@Inject
